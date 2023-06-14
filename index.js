7e2607a50948fc8272bc8a25a817c70f708d3d0b
njugudev <njugudev@pop-os.localdomain>
2023-06-14 15:45:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // code your solution here
+function saturdayFun(a = 'roller-skate'){
+    return `This Saturday, I want to ${a}!`;
+
+ 
+}
+const passTime = saturdayFun("Tennis")
+passTime
+
+
+    
+
+function mondayWork(b = 'go to the office'){
+    return `This Monday, I will ${b}.`;
+       
+    
+}
+const getBusy = mondayWork("Chilling")
+getBusy
+
+
+
+
+
+
+
+
+
+
+
+function wrapAdjective(flair = "*"){
+    return function(mark = "special") {
+        return `You are ${flair}${mark}${flair}!`
+
+    }
+    
+    
+}
